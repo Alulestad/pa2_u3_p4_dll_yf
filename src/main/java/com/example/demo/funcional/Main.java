02ab84145cc6883293d31f5ccfbbed80f2024fe0
Alulestad <vladimirlestad@gmail.com>
2023-07-31 19:46:01 -0500

    taller34

--- src/main/java/com/example/demo/funcional/Main.java
+++ src/main/java/com/example/demo/funcional/Main.java
@@ -1,5 +1,7 @@
 package com.example.demo.funcional;
 
+import java.util.function.BiPredicate;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,7 +28,90 @@ public class Main {
 			return cedula;
 		};
 		LOG.info("Supplier lambda 2: " + supplier3.getId());
+		
+		//Metodos referenciados
+		MetodosReferenciados metodos=new MetodosReferenciados();
+		IPersonaSupplier<Integer> supplier4=metodos::getId;
+		LOG.info("Supplier metodo refenciado: " + supplier4.getId());
+		
 
+		// 2. Consumer
+		// Clases
+		IPersonaConsumer<String> consumer1 = new PersonaConsumerImpl();
+		LOG.info("Consumer clase");
+		consumer1.accept("Daniel y/o Yaniry");
+
+		// Lambdas:
+		IPersonaConsumer<String> consumer2 = cadena -> {
+			LOG.info("1");
+			LOG.info("2");
+			LOG.info(cadena);
+		};
+		LOG.info("Consumer lambda");
+		consumer2.accept("Daniel y/o Yaniry");
+		
+		//Metodos referenciados
+		IPersonaConsumer<String> consumer3=metodos::acceptar;
+		LOG.info("Consumer metodo referenciado");
+		consumer3.accept("Daniel3");
+		
+		
+		// 3.- Predicate
+		// Lambdas
+		IPersonaPredicate<Integer> predicate1 = valor -> valor.compareTo(8) == 0;
+		LOG.info("Predicate lambda: " + predicate1.evaluar(4));
+
+		IPersonaPredicate<Integer> predicate2 = valor -> {
+			Integer valor2 = 10;
+			valor = valor + valor2;
+			if (valor.compareTo(100) > 0) {
+				return true;
+			} else {
+				return false;
+			}
+		};
+		LOG.info("Predicate lambda 2: " + predicate2.evaluar(95));
+
+		IPersonaPredicate<String> predicate3 = letra -> "Daniel".contains(letra);
+		LOG.info("Predicate lambda 3: " + predicate3.evaluar("D"));
+
+		IPersonaBiPredicate<String, String> biPredicate = (nombre, letra) -> nombre.contains(letra);
+		LOG.info("BiPredicate lambda: " + biPredicate.evaluar("Daniel", "D"));
+
+		
+		//Metodos referenciados
+		IPersonaPredicate<Integer> predicate5=metodos::evaluar;
+		LOG.info("Predicate metodo referenciado: " + predicate5.evaluar(-5));
+		
+		
+		
+		// 4.- Function
+		// Lambdas
+		IPersonaFunction<String, Integer> function1 = numero -> {
+			String valorFinal = numero.toString().concat("valor");
+			return valorFinal;
+		};
+		LOG.info("Function lambda: " + function1.aplicar(10));
+		
+		
+		//Metodos referenciados
+		IPersonaFunction<Integer, String> function2=metodos::aplicar;
+		LOG.info("Function metodo referenciado: " + function2.aplicar("100"));
+		
+		
+		// 5.- Unary Operator
+		// Lambdas
+		IPersonaUnaryOperator<Integer> unaryOperator=numero->numero+(numero*2);
+		LOG.info("Unary Operator lambda: " + unaryOperator.aplicar(3));
+		
+		IPersonaUnaryOperatorFunction<Integer> unaryOperatorFunction=numero->numero+(numero*2);
+		LOG.info("Unary Operator Function lambda: " + unaryOperatorFunction.aplicar(3));
+		
+		
+		IPersonaUnaryOperator<Integer> unaryOperator2=metodos::aplicar2;
+		LOG.info("Unary Operator metodo referenciado: " + unaryOperator2.aplicar(3));
+		
+		
 	}
 
 }
---
+++ src/main/java/com/example/demo/funcional/MetodosReferenciados.java
@@ -0,0 +1,36 @@
+package com.example.demo.funcional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MetodosReferenciados {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(Main.class); // slf4j
+
+	
+	public Integer getId() {
+		return 8;
+	}
+	
+	
+	public void acceptar(String arg) {
+		String cadena="Daniel y Yaniry";
+		
+		LOG.info(cadena+" "+arg);
+	}
+	
+	public boolean evaluar(Integer numero) {
+		
+		
+		return numero>=0;
+	}
+	
+	public Integer aplicar(String cadena) {
+		
+		return Integer.valueOf(cadena);
+	}
+	
+	public Integer aplicar2(Integer valor) {
+		return valor*100;
+	}
+}
